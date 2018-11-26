1e4ec065ae44c88fecbfcd4f853e205fdcaa6313
huangqi <huang.qi@vpclub.cn>
2018-11-26 17:43:19 +0800

    登陆页的跳转优化

--- miniprogram/pages/login/login.js
+++ miniprogram/pages/login/login.js
@@ -5,18 +5,7 @@ Page({
   },
   //页面加载时触发。一个页面只会调用一次
   onLoad: function() {
-    /*定义微信全局的权限请求,session为家长端的访问控制 token是机构端的访问控制
-      在登录小程序时初始化session,在机构登录时初始化token
-    */
-    wx.Global = {
-      domain: "http://127.0.0.1:8080",
-      user: "",
-      header: {
-        'content-type': 'application/json;charset=UTF-8',
-        'token': '',
-        'session': ''
-      }
-    }
+   
     //用于全局发起带权限的请求
     wx.AuthPostReq = function(obj) {
       obj.url = wx.Global.domain + obj.url
@@ -31,24 +20,6 @@ Page({
       obj.method = "GET"
       wx.request(obj)
     }
-    // 获取用户信息
-    wx.getSetting({
-      success: res => {
-        if (res.authSetting['scope.userInfo']) {
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-          wx.getUserInfo({
-            "withCredentials": true,
-            "success": function(data) {
-              //初始化全局user主体
-              wx.Global.user = data
-              //执行登录操作
-              lgoinApp()
-            },
-            "fail": res
-          })
-        }
-      }
-    })
   },
   //用户授权登录后的回调函数
   authCallback: function(data) {
